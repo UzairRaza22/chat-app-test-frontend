90a7fdbc5ca61e68d4da259edfcdadbbdcd1c184
UzairRaza22 <uzairraza188@gmail.com>
2026-04-01 12:12:47 +0500

    intial commit

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,32 @@
+<template>
+  <v-app>
+    <v-main>
+      <div style="padding: 20px">
+        <h2>Socket Test App</h2>
+
+        <p>Status: {{ connected ? '🟢 Connected' : '🔴 Disconnected' }}</p>
+
+        <input v-model="form.text" placeholder="Message" />
+        <input v-model="form.sender_id" placeholder="Sender ID" />
+
+        <button @click="sendMessage" :disabled="loading">Send</button>
+
+        <p style="color: green">{{ successMessage }}</p>
+        <p style="color: red">{{ errorMessage }}</p>
+
+        <hr />
+
+        <h3>Socket Messages</h3>
+        <ul>
+          <li v-for="(msg, index) in socketMessages" :key="index">
+            {{ msg.text }} - {{ msg.sender_id }} ({{ msg.timestamp }})
+          </li>
+        </ul>
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue'
 import axios from 'axios'
@@ -10,7 +39,6 @@ const errorMessage = ref('')
 const socketMessages = ref([])
 const connected = ref(false)
 
-// Create socket once outside onMounted
 const socket = io('http://178.104.58.236', {
   path: '/test-socket/socket.io',
   transports: ['polling', 'websocket'],
@@ -23,7 +51,6 @@ onMounted(() => {
   })
 
   socket.on('test_message_received', (data) => {
-    console.log('📩 Socket event received:', data)
     socketMessages.value.unshift({
       text: data.text,
       sender_id: data.sender_id,
